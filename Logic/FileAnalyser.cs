3115ab2b41a065c726cc866a26d1590d717ee617
sasha15455 <sasha15452@gmail.com>
2021-04-03 20:28:30 +0300

    find string occurrences in file added

---
+++ Logic/FileAnalyser.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.FileParcer.Logic
+{
+    class FileAnalyser
+    {
+        public int ParceFile(string fileName, string searchInFile, string replaceInFile)
+        {
+            string tempFileName = string.Format("{0}{1}.txt", Path.GetTempPath(), Guid.NewGuid().ToString());
+            int count = 0;
+
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    List<string> list = new List<string>();
+                    //List<string> list2 = new List<string>();
+
+                    //list2 = File.ReadLines(Path.Combine);
+
+                   
+
+                    while (sr.Peek() != -1)
+                    {
+                        string line = sr.ReadLine();
+
+                        if (line.Contains(searchInFile))
+                        {
+                            line = line.Replace(searchInFile, replaceInFile);
+                            count++;
+                        }
+
+                        list.Add(line);
+
+                        if (list.Count == 10000)
+                        {
+                            File.AppendAllLines(tempFileName, list);
+                            list.Clear();
+                        }
+                    }
+
+                    File.AppendAllLines(tempFileName, list);
+                }
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                File.Move(tempFileName, path);
+
+                return count;
+            }
+            catch (Exception) //ToDO:
+            {
+
+                throw;
+            }
+            finally
+            {
+                File.Delete(tempFileName);
+            }
+        }
+
+        public int CountOccurrencesInFile(string fileName, string searchInFile)
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            return File.ReadLines(Path.Combine(Directory.GetCurrentDirectory(), fileName))
+                   .Where(l => l.Contains(searchInFile)).Count();
+        }
+    } 
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using _4.FileParcer.Logic;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +13,37 @@ namespace _4.FileParcer
     {
         static void Main(string[] args)
         {
-            FileHandler fileUser = new FileHandler();
+            Stopwatch stopwatch = new Stopwatch();
+            // Запускаем внутренний таймер объекта Stopwatch
+            
+            FileAnalyser fileUser = new FileAnalyser();
+            
+            stopwatch.Start();
+            //int count = fileUser.CountOccurrencesInFile("file2.txt", "ABCDEFG");
+            int count = fileUser.ParceFile("file.txt", "ABCDEFG", "QWERTYVCX");
+            stopwatch.Stop();
 
-            fileUser.OpenOrCreate().GetAwaiter().GetResult();
+            //IncreaseSize();
+            Console.WriteLine("Потрачено миллисекунд на выполнение: " + stopwatch.ElapsedMilliseconds);
+            //Console.WriteLine("Количество вхождений: " + count);
+            //Console.WriteLine("Потреблено памяти: " + consumedInMegabytes);
+            Console.WriteLine("Количество замен: " + count);
+
+            
+
             Console.ReadKey();
+        }
+
+        public static void IncreaseFileSize()
+        {
+            using (StreamReader sr = new StreamReader("file.txt"))
+            {
+                string[] array;
+
+                array = File.ReadAllLines("file.txt");
+
+                File.AppendAllLines("file2.txt", array);
+            }
         }
     }
 }
