75f16e06129cb264eada003bc4d89f57df47424e
sasha15455 <sasha15452@gmail.com>
2021-04-03 14:23:29 +0300

    Basic replacing strings logic added

---
+++ Logic/FileHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _4.FileParcer.Logic
+{
+    class FileHandler
+    {
+        public async Task OpenOrCreate()
+        {
+            string fileName = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ".txt";
+
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "file.txt");
+
+                string str = "o";
+                string subStr = "AA";
+
+                using (StreamWriter sw = new StreamWriter(fileName))
+                {
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        while (sr.Peek() != -1)
+                        {
+                            string line = sr.ReadLine();
+
+                            if (line.Contains(str))
+                            {
+                                line = line.Replace(str, subStr);
+                            }
+
+                            sw.WriteLine(line);
+                        }
+                    }
+                }
+
+                string str23 = "path to output file.txt";
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                File.Move(fileName, path);
+
+                //File.Replace(str23, path, null);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                File.Delete(fileName);
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _4.FileParcer.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,10 @@ namespace _4.FileParcer
     {
         static void Main(string[] args)
         {
+            FileHandler fileUser = new FileHandler();
+
+            fileUser.OpenOrCreate().GetAwaiter().GetResult();
+            Console.ReadKey();
         }
     }
 }
